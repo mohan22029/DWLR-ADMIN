ae49a0fcb715b5f24c9f689fdc53080701ac5b1c
mohan22029 <moha22029.ad@rmkec.ac.in>
2025-05-18 16:48:45 +0530

    anomaly page

--- src/components/Anomaly.js
+++ src/components/Anomaly.js
@@ -5,7 +5,7 @@ const AnomalyFeature = () => {
   return (
     <div style={{ padding: '20px', height: '100%', overflow: 'auto' }}>
       <iframe
-        src="https://frontend-anomaly.vercel.app"
+        src="https://frontend-anomaly-7el4.vercel.app"
         style={{ width: '100%', height: '80vh', border: 'none', borderRadius: '8px' }}
         title="Anomaly Feature"
       />
